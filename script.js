69fd7c7ada573e350f200628c11b4ea321893419
Moeez Khalil <khalilqaiser71@gmail.com>
2026-05-19 12:09:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let guessInput = document.getElementById('number');
+let check = document.getElementById('check');
+let result = document.getElementById('result');
+let restart = document.getElementById('restart');
+
+let randomNumber = Math.floor(Math.random() * 100) + 1
+
+check.addEventListener('click' , ()=>{
+    let guess = Number(guessInput.value);
+
+    if (guess < 1 || guess > 100) {
+        result.textContent = "Enter a number between 1 and 100.";
+        result.style.color = "red";
+    }
+    else if(guess === randomNumber){
+        result.textContent = 'Congrats! 🎉 You guessed it Right!';
+        result.style.color = 'green';
+        check.disabled = true;
+        restart.style.display = 'block';
+    }
+   else {
+        result.textContent = guess < randomNumber ? "Too low! Try again." : "Too high! Try again.";
+        result.style.color = "red";
+    }
+
+   guessInput.value = "";   
+    guessInput.focus();
+
+    restart.addEventListener('click' , ()=>{
+        randomNumber = Math.floor(Math.random() * 100) + 1
+        result.textContent = "";
+        check.disabled = false;
+        restart.style.display = "none";
+        guessInput.focus();
+    })
+})
